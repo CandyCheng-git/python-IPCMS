8c0b741056fa3ce341e27660709164ff96fb008b
CandyCheng-git <loktung06cheng@gmail.com>
2024-10-16 13:49:00 +1100

    Updated logging

--- reports/reports.py
+++ reports/reports.py
@@ -164,4 +164,5 @@ class ReportGenerator:
         ms_word_doc = Document()
         self.create_report_document(ms_word_doc, report_contents)
         ms_word_doc.save(set_filename)
-        logging.info(f'Report "{set_filename}" created successfully. \nLocation: "{get_cur_location()}\\{set_filename}"')
+        logging.info(f'Report "{set_filename}" created successfully. '
+                     f'\nLocation: "{get_cur_location()}\\{set_filename}"')
